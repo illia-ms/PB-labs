e2d13a2ad20329bdfae38ab2a7a98361ff8b8952
Illia Mashtaler <elijah.mashtaler@gmail.com>
2021-09-27 18:37:38 +0300

    final version

--- Лабораторна робота №1/javaCode/src/com/lab/Main.java
+++ Лабораторна робота №1/javaCode/src/com/lab/Main.java
@@ -6,10 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Задайте значення кута β у градусах: ");
-        float beta = scanner.nextFloat();
-        float rad = (float) ((beta * Math.PI) / 180);
-        System.out.println("Радіанна міра кута β: " + rad);
+        System.out.println("Задайте значення кута β у градусах:");
+        float beta = scanner.nextFloat();   //Введення градусної міри beta
+        float rad = (float) ((beta * Math.PI) / 180);   //Обчислення радіанної міри beta
+        System.out.println("Радіанна міра кута β: " + rad); //Вивід результату
         scanner.close();
     }
 }
+
+
+
